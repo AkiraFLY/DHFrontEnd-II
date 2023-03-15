d9038ff653661cd38958ecb2a3441f8b67a9442e
Akira <akiray1997@gmail.com>
2023-03-15 19:27:56 -0300

    Atualizando Checkpoint I

--- Front End II/Consult/Checkpoint/main.js
+++ Front End II/Consult/Checkpoint/main.js
@@ -1,52 +1,168 @@
-const inputNameRef = document.querySelector('#taskName')
-const inputUrlRef = document.querySelector('#url')
-const inputDescRef = document.querySelector('#description')
-const inputButtonRef = document.querySelector('#createTaskButton')
-const taskCardRef = document.querySelector('#taskCards')
+const taskNameRef = document.querySelector('#taskName')
+const urlRef = document.querySelector('#url')
+const descriptionRef = document.querySelector('#description')
+const createTaskButtonRef = document.querySelector('#createTaskButton')
+const taskCardsRef = document.querySelector('#taskCards')
 
-var formError = {
-  taskName: true,
-  url: true,
-  description: true
 
-}
+const cards = []
 
-function checkFormValidity(){
+
+createTaskButtonRef.disabled = true
+createTaskButtonRef.classList.add('button_error')
+
+
+
+function validaTitulo(titulo, allowEmpty=false){
+  let errorMessageRef = document.querySelector('.input_titulo') 
+  let taskNameRef = document.querySelector('#taskName')
+  let errorMessage = document.createElement('p')
   
-  const formErrorArray = Object.values(formErros)
+  console.log(titulo)
+  titulo = titulo.trim()
 
-  const formValidity = formErrorArry.every(item => item === false)
-
-  inputButtonRef.desable =!formValidity
-
-
+  if (titulo === '') {
+    if (errorMessageRef.querySelector('#taskErrorMessage')) {
+      errorMessageRef.removeChild(errorMessageRef.querySelector('#taskErrorMessage'));
+    }
+    errorMessage.id = 'taskErrorMessage'
+    errorMessage.innerText = '*Por favor, preencha o campo Título, ele não pode ficar em branco ou conter apenas espaços'
+    errorMessageRef.appendChild(errorMessage)
+    taskNameRef.classList.add('error')
+    createTaskButtonRef.classList.add('button_error')
+    createTaskButtonRef.disabled = true
+  return  
+  }else{
+    if(errorMessageRef.querySelector('#taskErrorMessage')){
+      errorMessageRef.removeChild(errorMessageRef.querySelector('#taskErrorMessage'))
+    }
+    taskNameRef.classList.remove('error')
+    createTaskButtonRef.classList.remove('button_error')
+    createTaskButtonRef.disabled = false
+  }
 }
 
 
-function validateInput(inputRef){
+function validaUrl(url, allowEmpty=false){
+  let errorMessageRef = document.querySelector('.input_url') 
+  let urlRef = document.querySelector('#url')
+  let errorMessage = document.createElement('p')
 
-  const inputValid = inputRef.checkFormValidity()
-  const elementFatherRef = inputRef.parentElement
-
-  if(inputValid){
-    elementFatherRef.classList.remove('error_message_container')
-  }else{
-
-    elementFatherRef.classList.add('error_message_container')
+    if (url === '') {
+    if (errorMessageRef.querySelector('#taskErrorMessage')) {
+      errorMessageRef.removeChild(errorMessageRef.querySelector('#taskErrorMessage'));
+    }
+    errorMessage.id = 'taskErrorMessage'
+    errorMessage.innerText = '*Por favor, preencha o campo URL'
+    errorMessageRef.appendChild(errorMessage)
+    urlRef.classList.add('error')
+    createTaskButtonRef.classList.add('button_error')
+    createTaskButtonRef.disabled = true
+  return  
   }
 
-  formError[inputRef.id] = !inputValid
-  checkFormValidity()
+  let regex = new RegExp("^((((https?|ftp|gopher|telnet|nntp):(\\/\\/)|(http|https):\\/\\/)?([\\da-z.-]+)\\.([a-z.]{2,6})|((mailto|news):(\\/\\/)|(http|https):\\/\\/)([a-z0-9-]+(\\.[a-z0-9-]+)*\\.[a-z]{2,6}))(:\\d{2,5})?([\\/\\w.-]*)*\\/?(\\?([\\w\\-.,@?^=%&:\\/~+#]*)+)?(#([\\w\\-\\s]*)+)?)?$", "i");
+  
+  if (!regex.test(url)) {
+    if (errorMessageRef.querySelector('#taskErrorMessage')) {
+      errorMessageRef.removeChild(errorMessageRef.querySelector('#taskErrorMessage'));
+    }
+  if (!errorMessageRef.querySelector('#taskErrorMessage')) {
+    errorMessage.id = 'taskErrorMessage'
+    errorMessage.innerText = '*Por favor, preencha corretamente o campo URL, com uma url completa'
+    errorMessageRef.appendChild(errorMessage)
+  }
+    urlRef.classList.add('error')
+    createTaskButtonRef.classList.add('button_error')
+    createTaskButtonRef.disabled = true
+  }else{
+    if(errorMessageRef.querySelector('#taskErrorMessage')){
+      errorMessageRef.removeChild(errorMessageRef.querySelector('#taskErrorMessage'))
+    }
+    urlRef.classList.remove('error')
+    createTaskButtonRef.classList.remove('button_error')
+    createTaskButtonRef.disabled = false
+  }
+  
+  }
 
+
+
+
+function validaDescricao(descricao, allowEmpty=false){
+  let errorMessageRef = document.querySelector('.input_descricao') 
+  let descriptionRef = document.querySelector('#description')
+  let errorMessage = document.createElement('p')
+
+  descricao = descricao.trim()
+
+  if (descricao === '') {
+    if (errorMessageRef.querySelector('#taskErrorMessage')) {
+      errorMessageRef.removeChild(errorMessageRef.querySelector('#taskErrorMessage'));
+    }
+    errorMessage.id = 'taskErrorMessage'
+    errorMessage.innerText = '*Por favor, preencha o campo Descrição, ele não pode ficam em branco ou conter apenas espaços'
+    errorMessageRef.appendChild(errorMessage)
+    descriptionRef.classList.add('error')
+    createTaskButtonRef.classList.add('button_error')
+    createTaskButtonRef.disabled = true
+  return  
+  }else{
+    if(errorMessageRef.querySelector('#taskErrorMessage')){
+      errorMessageRef.removeChild(errorMessageRef.querySelector('#taskErrorMessage'))
+    }
+    descriptionRef.classList.remove('error')
+    createTaskButtonRef.classList.remove('button_error')
+    createTaskButtonRef.disabled = false
+  }
 
 }
-
 
 function newTask(event){
   event.preventDefault()
+
+  if (taskNameRef.value && urlRef.value && descriptionRef.value) {
+
+let card = {
+  titulo: taskNameRef.value,
+  url: urlRef.value,
+  descricao: descriptionRef.value
+ }
+
+validaTitulo(taskNameRef.value)
+validaUrl(urlRef.value)
+validaDescricao(descriptionRef.value)
+
+
+ cards.push(card)
+
+ taskCardsRef.innerHTML += `
+ <div class="card">
+ <img src="${card.url}" alt="">
+ <h3> ${card.titulo} </h3>
+ <p> ${card.descricao} </p>
+</div>
+`
+taskNameRef.value = ''
+urlRef.value = ''
+descriptionRef.value = ''
+
+createTaskButtonRef.disabled = true
+createTaskButtonRef.classList.add('button_error')
+
+}else{
+createTaskButtonRef.disabled = true
+createTaskButtonRef.classList.add('button_error')
+validaTitulo(taskNameRef.value)
+validaUrl(urlRef.value)
+validaDescricao(descriptionRef.value)
+}
 }
 
 
-inputNameRef.addEventListener ('keyup', () => validateInput(inputNameRef))
-inputUrlRef.addEventListener ('keyup', () => validateInput(inputUrlRef))
-inputDescRef.addEventListener ('keyup', (event) => newTask(event))
+
+taskNameRef.addEventListener('keyup', (event) => validaTitulo(event.target.value))
+urlRef.addEventListener('keyup', (event) => validaUrl(event.target.value))
+descriptionRef.addEventListener('keyup', (event) => validaDescricao(event.target.value))
+createTaskButtonRef.addEventListener('click', (event) => newTask(event))
+
